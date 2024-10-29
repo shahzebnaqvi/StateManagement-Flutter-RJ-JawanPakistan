05aa4d93a3576fae77170f68f7625c0a90cbc17b
hamzakamelen <hamzakamelen12345@gmail.com>
2024-10-29 20:27:30 +0500

    basic structure flutter

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _count = 0;
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    print("calling build method");
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "$_count",
+          style: TextStyle(
+            fontSize: 30,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _count++;
+          print("Tapping increment button and Count value $_count");
+          setState(() {});
+        },
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
